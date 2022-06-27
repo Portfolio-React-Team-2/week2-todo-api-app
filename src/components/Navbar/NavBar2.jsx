c112fbf2b580d114ea51497c2885f76ed6c7aec8
roji-tech <rojitech9@gmail.com>
2022-06-27 11:37:43 +0100

    added reports

--- src/components/Navbar/NavBar2.jsx
+++ src/components/Navbar/NavBar2.jsx
@@ -1,27 +1,48 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../images/logo.svg";
 import NavStyles from "./navbarStyles";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [stickyNavbar, setStickyNavbar] = useState("static");
+  const navBarRef = useRef();
+
+  const openCloseNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 80) {
+        setStickyNavbar("fixed");
+      } else {
+        setStickyNavbar("static");
+      }
+    });
+  }, []);
+
+// style={{ position: stickyNavbar }}
+
   return (
-    <NavStyles>
+    <NavStyles >
+      <input type="checkbox" name="navToggle" id="navToggle" />
       <div className="logo">
         <Link to={"/"}>
           <img src={Logo} alt="todo-app" />
         </Link>
 
         <div className="strokes-container">
-          <div className="strokes">
-            <div className="stroke"></div>
-            <div className="stroke"></div>
-            <div className="stroke"></div>
-          </div>
+          <label htmlFor="navToggle">
+            <div className="strokes" onClick={openCloseNav}>
+              <div className="stroke"></div>
+              <div className="stroke"></div>
+              <div className="stroke"></div>
+            </div>
+          </label>
         </div>
       </div>
       <div className="lists">
-        <div></div>
-
         <ul>
           <li>
             <Link to={"/"}>
--- src/components/Navbar/navbarStyles.js
+++ src/components/Navbar/navbarStyles.js
@@ -9,6 +9,13 @@ const NavStyles = styled.div`
   background-color: var(--bg-light);
   padding: 10px 5%;
 
+  input#navToggle {
+    position: absolute;
+    left: -200px;
+    display: none;
+    appearance: none;
+  }
+
   div.lists {
     display: flex;
     justify-content: space-between;
@@ -48,18 +55,28 @@ const NavStyles = styled.div`
     }
   }
 
+  @media screen and (max-width: 1200px) {
+    .lists > ul > li {
+      font-size: 0.8em;
+    }
+  }
+
   @media screen and (max-width: 1000px) {
     display: flex;
     flex-direction: column;
     background-color: lightcyan;
-    padding: 10px 5%;
-    position: relative;
+    padding: 10px 0;
 
     div.logo {
-      width: 100%;
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      /* background-color: gray; */
+      width: 70%;
+      display: flex;
+      justify-content: space-between;
+      margin: 0 10%;
+      /* grid-template-columns: 1fr 1fr; */
+
+      /* @media screen and (max-width: 800px) {
+        justify-content: space-around;
+      } */
 
       div.strokes-container {
         display: flex;
@@ -69,47 +86,71 @@ const NavStyles = styled.div`
           padding: 0;
           margin: 0;
         }
+
+        input#navToggle {
+          position: absolute;
+          left: -200px;
+          display: none;
+          appearance: none;
+        }
+
         div.strokes {
+          box-sizing: border-box;
           display: flex;
+          border-radius: 50%;
+          aspect-ratio: 1/1;
+          overflow: hidden;
           flex-direction: column;
           gap: 3px;
           width: max-content;
           justify-content: space-around;
-          padding: 5px;
-          /* height: max-content; */
-          /* background-color: blue; */
+          margin: 5px;
           aspect-ratio: 1/1;
+          transition: all 0.45s ease-in-out;
+          z-index: 5;
+
+          @media screen and (max-width: 750px) {
+            /* transform: rotate(90deg); */
+            /* background-color: gray; */
+          }
 
           div.stroke {
-            width: 60px;
-            height: 5px;
+            width: 50px;
+            height: 8px;
             background-color: var(--btn-try);
-            /* align-self: flex-end;
-        justify-self: flex-end; */
+
+            /* @media screen and (max-width: 500px) {
+              width: 40px;
+            } */
           }
         }
       }
     }
 
     div.lists {
-      /* position: ; */
-      /* top: 0;
-      left: 0; */
-      padding: 20px 0;
-      width: 90%;
-      height: 100%;
+      /* position: sticky; */
+      /* top: -1000; */
+      width: 100%;
+      height: 0;
       display: flex;
-      /* flex-direction: column; */
-      justify-content: space-between;
+      flex-direction: column;
+      justify-content: center;
       align-items: center;
-      background-color: yellow;
+      gap: 60px;
+      overflow: hidden;
+      /* background-color: red; */
+      transition: height 4s linear;
+      transition-property: height;
 
       ul {
         width: 100%;
         display: flex;
         flex-direction: column;
+        justify-content: center;
+        align-items: center;
         gap: 35px;
         list-style: none;
+        padding: 20px 0;
 
         a {
           color: black;
@@ -119,7 +160,6 @@ const NavStyles = styled.div`
 
       div.nav--right {
         display: flex;
-        flex-direction: column-reverse;
         align-items: center;
         gap: 50px;
 
@@ -141,6 +181,15 @@ const NavStyles = styled.div`
         }
       }
     }
+
+    input#navToggle:checked ~ .lists {
+      height: 100%;
+    }
+
+    /* #open {
+      height: 100%;
+    } */
   }
 `;
+
 export default NavStyles;
